bbc4d8b46319764b72f98775c85e68e31538a406
shiivvvaam <100465805+shiivvvaam@users.noreply.github.com>
2024-01-29 21:25:13 +0530

    Update setup.py

--- Topsis_tewatia-master/setup.py
+++ Topsis_tewatia-master/setup.py
@@ -1,30 +1,27 @@
 
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
-    name="Topsis",
-    version="1.0.4",
-    author="Yash Kumar Tewatia",
-    author_email="yashbsr3@gmail.com",
-    description="A package -> Calculates Topsis Score and Rank them accordingly",
+    name="Topsis_Shivam_102116113",
+    version="1.0.0",
+    author="Shivam Dhiman",
+    author_email="shivam4968@gmail.com",
+    description="It Calculates Topsis Score and Rank",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    url="https://github.com/Tewatia5355/Topsis_tewatia",
+    url="https://github.com/shiivvvaam/Topsis",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    packages=["Topsis_Tewatia"],
+    packages=["Topsis_Shivam_102116113"],
     include_package_data=True,
     install_requires='pandas',
     entry_points={
         "console_scripts": [
-            "topsis=Topsis_Tewatia.topsis:main",
+            "topsis=Topsis_Shivam_102116113.topsis:main",
         ]
     },
 )
